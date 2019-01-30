f6963f395331d5b25004dd7c06159c1afcae3238
Mike Donovan <caffeine239@yahoo.com>
2019-01-29 20:04:42 -0500

    Update SongLoader.cs
    Removed old code.

--- Assets/Scripts/SongLoader.cs
+++ Assets/Scripts/SongLoader.cs
@@ -133,62 +133,8 @@ public class SongLoader : MonoBehaviour
             }
         }
 
-
-
-
-        /*Song.Audio audio = new Song.Audio();
-		FileInfo guitarFileInfo = new FileInfo(song.fileInfo.Directory.FullName + "/guitar.ogg");
-		if (guitarFileInfo.Exists)
-		{
-			using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(guitarFileInfo.FullName, AudioType.OGGVORBIS))
-			{
-				yield return uwr.SendWebRequest();
-				if (uwr.isNetworkError || uwr.isHttpError)
-				{
-					Debug.LogError(uwr.error);
-					yield break;
-				}
-				yield return null;
-				audio.guitar = DownloadHandlerAudioClip.GetContent(uwr);
-			}
-		}
-		Debug.Log("Loading song");
-		yield return null;
-		FileInfo songFileInfo = new FileInfo(song.fileInfo.Directory.FullName + "/song.ogg");
-		if (songFileInfo.Exists)
-		{
-			using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(songFileInfo.FullName, AudioType.OGGVORBIS))
-			{
-				yield return uwr.SendWebRequest();
-				if (uwr.isNetworkError || uwr.isHttpError)
-				{
-					Debug.LogError(uwr.error);
-					yield break;
-				}
-				yield return null;
-				audio.song = DownloadHandlerAudioClip.GetContent(uwr);
-			}
-		}
-		Debug.Log("Loading rhythm");
-		yield return null;
-		FileInfo rhythmFileInfo = new FileInfo(song.fileInfo.Directory.FullName + "/rhythm.ogg");
-		if (rhythmFileInfo.Exists)
-		{
-			using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(rhythmFileInfo.FullName, AudioType.OGGVORBIS))
-			{
-				yield return uwr.SendWebRequest();
-				if (uwr.isNetworkError || uwr.isHttpError)
-				{
-					Debug.LogError(uwr.error);
-					yield break;
-				}
-				yield return null;
-				audio.rhythm = DownloadHandlerAudioClip.GetContent(uwr);
-			}
-		}*/
-		//song.audio = audio;
-		Debug.Log("Audio loaded");
-		onPrepared();
+	Debug.Log("Audio loaded");
+	onPrepared();
 	}
 
 	
